ad3d44fdb24e0c233622655f7531a8600e8aa784
vn218 <72967949+vn218@users.noreply.github.com>
2021-08-01 12:19:49 +0530

    Update display_bin.m

--- matlab/display_bin.m
+++ matlab/display_bin.m
@@ -1,5 +1,11 @@
 file = fopen('dwt.bin', 'r');
 a = fread(file,256*256,'uint8=>uint8');
-a = reshape(a,256,256);
-imshow(a); 
+k = 1;
+for i = 1:256
+    for j = 1:256
+        b(i,j) = a(k);
+        k = k + 1;
+    end
+end
+imshow(b); 
 fclose(file);
